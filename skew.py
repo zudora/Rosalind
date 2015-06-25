8ba71bf3333539bfb33324eea7025cd31747ac9b
zudora <zudora@users.noreply.github.com>
2015-06-24 21:31:22 -0700

    Skew finished

--- skew.py
+++ skew.py
@@ -1,6 +1,5 @@
 #CATGGGCATCGGCCATACGCC
 #Give all values of Skew-sub-i (GAGCCACCGCGATA) for i ranging from 0 to 14.
-#    0 -1 -1 -1 0 1 2 1 1 1 0 1 2 1 0 0 0 0 -1 0 -1 -2
 
 # We can compute Skewi+1(Genome) from Skewi(Genome) according to the nucleotide in position i of Genome. 
 #   If this nucleotide is G, then Skewi+1(Genome) = Skewi(Genome) + 1
@@ -8,20 +7,28 @@
 #   otherwise, Skewi+1(Genome) = Skewi(Genome).
 
 def skew(genome, irange):
-    skew_list = []
-    for i in range(0, irange):
+    skew_i = 0
+    skew_list = [0]
+    for i in range(0, len(genome)):        
         nuc = genome[i]
         if nuc == "G":
-            pass
+            skew_i += 1
         elif nuc == "C":
-            pass
+            skew_i -= 1
         else:
             pass
+        skew_list.append(skew_i)        
     return skew_list
 
 
-genome = "CATGGGCATCGGCCATACGCC"
+genome = "GAGCCACCGCGATA"
+#   Expected
+#   [0, -1, -1, -1, 0, 1, 2, 1, 1, 1, 0, 1, 2, 1, 0, 0, 0, 0, -1, 0, -1, -2]
+#   
+#   [0, -1, -1, -1, 0, 1, 2, 1, 1, 1, 0, 1, 2, 1, 0, 0, 0, 0, -1, 0, -1, -2]   
+
 irange = 14
 
 skew_list = skew(genome, irange)
+print skew_list
 
